a80aa161fbe4df5d3609ea6fea8069f8bacad75b
FesonX <fesonx@foxmail.com>
2020-06-08 23:13:48 +0800

    feature: Article editor route and backend

---
+++ src/dao.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 _*-  
+""" 
+@file: dao.py 
+@time: 2020-06-07 10:21
+@author:FesonX 
+@contact: fesonx@foxmail.com
+@site: github.com/FesonX 
+@software: PyCharm 
+"""
+from src.models import Article
+from src import db
+from loguru import logger
+from collections import Sequence
+
+
+def update():
+    try:
+        db.session.commmit()
+    except Exception as e:
+        db.session.rollback()
+        logger.exception(e)
+
+
+def save_instances(instances: Sequence):
+    try:
+        db.session.bulk_save_objects(instances)
+        db.session.commit()
+    except Exception as e:
+        logger.exception(e)
+
+
+def get_articles(query_conditions, fields=None, page: int = 1, per_page: int = 20):
+    if fields:
+        fields = [getattr(field) for field in fields]
+    base_query = db.session.query(fields or Article)
+    if query_conditions:
+        base_query = base_query.filter(*query_conditions)
+    return base_query.offset(page).limit(per_page)
+
+
+def get_article(query_conditions, fields=None):
+    if fields:
+        fields = [getattr(field) for field in fields]
+    return db.session.query(fields or Article).filter(*query_conditions).first()
--- src/views/common_views.py
+++ src/views/common_views.py
@@ -7,10 +7,39 @@
 @site: github.com/FesonX 
 @software: PyCharm 
 """
-from flask import render_template
+from flask import render_template, request, jsonify, session, g
+
 from src import app
+from src.controllers import request_validator
+from src.dao import save_instances
+from src.forms import ArticleForm
+from src.models import ArticleTag, Article
 
 
 @app.route('/')
 def index():
-    return render_template('base.html', cdn_domain='https://cdn.staticfile.org', title="FesonX's Blog")
+    return render_template('index.html', cdn_domain='https://cdn.staticfile.org', title="FesonX's Blog")
+
+
+@app.route('/editor', methods=['GET'])
+def editor_manage():
+    if request.method == 'GET':
+        return render_template('article_manage.html', cdn_domain='https://cdn.staticfile.org', title="FesonX's Blog")
+
+
+@app.route('/articles', methods=['POST'])
+@request_validator(ArticleForm)
+def article_manage():
+    if request.method == 'POST':
+        req_form: ArticleForm = g.my_form
+        author = req_form.author.data or session.get('user_id')
+        instances = list()
+        article = Article(username=author, title=req_form.title.data,
+                          body=req_form.body.data,
+                          section=req_form.column.data, summary=req_form.summary.data or req_form.body.data[:50])
+        instances.append(article)
+        if req_form.tags.data:
+            tags = [ArticleTag(tag=t, username=author) for t in req_form.tags.data]
+            instances.extend(tags)
+        save_instances(instances)
+        return jsonify({"msg": "Success", "code": 200})
